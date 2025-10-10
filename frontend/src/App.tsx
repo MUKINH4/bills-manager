f763392f5a230a04b2e646dd055d42dca5662744
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-10 12:00:50 -0300

    init: iniciado frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import SideBar from './components/SideBar'
+
+function App() {
+
+  return (
+    <>
+    <div className='grid grid-cols-3'>
+      <SideBar />
+
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/SideBar.tsx
@@ -0,0 +1,10 @@
+
+export default function SideBar() {
+    return (
+        <section className="h-screen">
+            <div>
+                <h1 className="text-sm">Gerenciador de Contas</h1>
+            </div>
+        </section>
+    );
+}
