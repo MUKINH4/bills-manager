570a28187a3046dd200bade678d9369993060646
MUKINH4 <samuelmaragato3@gmail.com>
2025-10-11 12:05:51 -0300

    feat: criacao de metodos de delete, put e get por id

--- api/src/main/java/com/bills_manager/api/service/BillService.java
+++ api/src/main/java/com/bills_manager/api/service/BillService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 import com.bills_manager.api.model.Bill;
 import com.bills_manager.api.repository.BillRepository;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class BillService {
     private final BillRepository billRepository;
@@ -21,5 +23,40 @@ public class BillService {
 
     public List<Bill> getAll() {
         return billRepository.findAll();
+    }
+
+    public Bill getBillById(int id) {
+        return billRepository.findById(id).orElseThrow(() ->
+            new EntityNotFoundException("ID não encontrado")
+        );
+    }
+
+    public void deleteBill(int id) {
+        Bill bill = getBillById(id);
+        billRepository.delete(bill);
+    }
+
+    public Bill editBill(int id, Bill bill) {
+        Bill existingBill = getBillById(id);
+        if (existingBill == null) {
+            throw new EntityNotFoundException("ID não encontrado");
+        }
+        existingBill.setAmount(bill.getAmount());
+        existingBill.setBillName(bill.getBillName());
+        existingBill.setCategory(bill.getCategory());
+        existingBill.setDueDate(bill.getDueDate());
+        existingBill.setPaid(bill.isPaid());
+
+        return billRepository.save(existingBill);
+    }
+
+    public void editBillPaidStatus(int id){
+        Bill bill = getBillById(id);
+        if (bill == null) {
+            throw new EntityNotFoundException("ID não encontrado");
+        }
+
+        bill.setPaid(!bill.isPaid());
+        billRepository.save(bill);
     }
 }
