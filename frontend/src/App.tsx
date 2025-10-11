1cde36cb21fd06bc2e1c98863e843640c3f5f909
MUKINH4 <samuelmaragato3@gmail.com>
2025-10-11 12:04:09 -0300

    feat: adicionado configuração de rotas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,36 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import SideBar from './components/SideBar'
+import type { SideBar as SideBarType } from './types/SideBar'
+import Dashboard from './pages/Dashboard'
+import AddBill from './pages/AddBill'
+
+let items: SideBarType[] = [
+  {
+    link: "/dashboard", name: "Dashboard"
+  },
+  {
+    link: "/bills", name: "Contas"
+  },
+  {
+    link: "/bills/add", name: "Adicionar Conta"
+  },
+]
+
 
 function App() {
 
   return (
-    <>
-    <div className='grid grid-cols-3'>
-      <SideBar />
+    <div className="flex">
+      <SideBar items={items} />
 
+      <main className="flex-1">
+        <Routes>
+          <Route index path='/dashboard' element={<Dashboard />} />
+          <Route index path='/bills/add' element={<AddBill />} />
+        </Routes>
+      </main>
     </div>
-    </>
   )
 }
 
